e50ad48ac9507141e3da2635c0b374b34f750e4a
TacticalLuke43 <luke.jeries@gmail.com>
2017-03-26 19:12:59 -0400

    Manage Buses Feature Added
    added screens and functionality to manage and add bus routes

--- app/src/main/java/csc383/busstopbuddy/ManageBuses.java
+++ app/src/main/java/csc383/busstopbuddy/ManageBuses.java
@@ -1,7 +1,11 @@
 package csc383.busstopbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ManageBuses extends AppCompatActivity {
 
@@ -9,5 +13,41 @@ public class ManageBuses extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_buses);
+
+    }
+    public void logOutButton(View view) {
+        Intent intent = new Intent(this, StartingScreen.class);
+        startActivity(intent);
+    }
+
+    public void newRouteButton(View view) {
+        Intent intent = new Intent(this, NewRoute.class);
+        startActivity(intent);
+    }
+    public void updateFormButton(View view) {
+        Spinner spinner = (Spinner) findViewById(R.id.routeSpinner);
+        String route1 = "Route 1";
+        String route2 = "Route 2";
+        if (String.valueOf(spinner.getSelectedItem()).equals(route1)) {
+            TextView departTime = (TextView) findViewById(R.id.departureTime);
+            departTime.setText("3:00 PM");
+            TextView estimateTime = (TextView) findViewById(R.id.estimatedRouteTime);
+            estimateTime.setText("40 Minutes");
+            TextView numberOfStops = (TextView) findViewById(R.id.numberOfStops);
+            numberOfStops.setText("3");
+            TextView stopsList = (TextView) findViewById(R.id.stopsTextView);
+            stopsList.setText("Stop 1: 111 Main Street \n Stop 2: 222 Main Street \n Stop 3: 333 Brown Road");
+        }
+        else if(String.valueOf(spinner.getSelectedItem()).equals(route2)){
+            TextView departTime = (TextView) findViewById(R.id.departureTime);
+            departTime.setText("3:10 PM");
+            TextView estimateTime = (TextView) findViewById(R.id.estimatedRouteTime);
+            estimateTime.setText("10 Minutes");
+            TextView numberOfStops = (TextView) findViewById(R.id.numberOfStops);
+            numberOfStops
+                    .setText("1");
+            TextView stopsList = (TextView) findViewById(R.id.stopsTextView);
+            stopsList.setText("Stop 1: 542 Leeroy Street");
+        }
     }
 }
